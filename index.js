413416719e5caf38ed1ee50a9d78e1abff8eb219
Ramiro Graca <valentinramirograca123@gmail.com>
2026-02-16 18:59:38 -0300

    Adding start command to upload it on the internet.

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const path = require("path");
 const app = express();
-const PORT = 4000;
+const PORT = process.env.PORT || 4000;
 
 //Allow parsing json files
 app.use(express.json());
